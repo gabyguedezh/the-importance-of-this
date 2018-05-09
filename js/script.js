ba097358d85e71d3bf903542cbd4080ed356e5ce
Gabriela Guedez Hamerlok <gaby.guedez@gmail.com>
2018-05-09 14:46:21 +0000

    created two buttons, added mouseenter and mouseleave functions

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
-$("p").click(function(){
-    $(this).slideToggle("slow");
-})
+// $("p").click(function(){
+//     $(this).slideToggle("slow");
+// });
+
+// Code above was to show how does THIS work
+
+$("button").mouseenter(function(){
+    $(this).removeClass("makeRed").addClass("makeBlue");
+});
+$("button").mouseleave(function(){
+    $(this).removeClass("makeBlue").addClass("makeRed");
+});
+
